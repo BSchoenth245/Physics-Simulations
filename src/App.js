3131b5863832604e661b355bf8ca38b8ac61beec
BSchoenth245 <brockschoenthaler@gmail.com>
2026-01-10 22:55:31 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Physics Simulations</h1>
+      <p>Welcome to your physics simulations project!</p>
+    </div>
+  );
+}
+
+export default App;
